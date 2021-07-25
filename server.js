0b1ce09294f638b5179455bd4079a2f72e8e67bc
Priya Samuel <p.samuel@elsevier.com>
2021-07-25 21:24:54 +0100

    Moving out MongoDB logic into separate module

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const session = require('express-session')
+const db = require('./db.js')
 const path = require('path')
 const app = express()
-const mongoose = require('mongoose')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({
@@ -17,37 +17,8 @@ app.use(session({
 
 app.set('view engine', 'pug')
 
-mongoose.Promise = global.Promise
-const db = {}
-db.mongoose = mongoose
-const dbConfig = require('./db.config.js')
-
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    user: dbConfig.USER,
-    pass: dbConfig.PASS,
-    authSource: dbConfig.DB
-  })
-  .then(() => {
-    console.log('Successfully connect to MongoDB.')
-  })
-  .catch(err => {
-    console.error('Connection error', err)
-    process.exit()
-  })
-
-const accountsSchema = new mongoose.Schema({
-  username: {
-    type: String
-  },
-  password: {
-    type: String
-  }
-})
-
-const accounts = mongoose.model('accounts', accountsSchema)
+db.connect()
+const accounts = db.accounts
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
